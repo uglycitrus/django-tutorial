d7d4ae38f9f0c27d111c2f16d5021498acdc0444
Will O'Meara <womeara@capsule.com>
2023-01-19 13:34:30 -0500

    add DRF - able to get user info in json
    first login here: http://localhost:8000/api-auth/login/
    then:
    ```
    curl 'http://localhost:8000/users/' \
            -H 'Accept:application/json' \
            -H 'Accept-Language: en-US,en;q=0.5' -H 'Accept-Encoding: gzip, deflate, br' \
            -H 'Connection: keep-alive' \
            -H 'Cookie: csrftoken=<TOKEN>; sessionid=<SESSION>' \
            -H 'Upgrade-Insecure-Requests: 1' \
            -H 'Sec-Fetch-Dest: document' \
            -H 'Sec-Fetch-Mode: navigate' \
            -H 'Sec-Fetch-Site: none' \
            -H 'Sec-Fetch-User: ?1' \
            -H 'Pragma: no-cache' \
            -H 'Cache-Control: no-cache'
    ```

--- wombo/urls.py
+++ wombo/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework import routers
+from wombo import views as wombo_views
+
+router = routers.DefaultRouter()
+router.register(r'users', wombo_views.UserViewSet)
+router.register(r'groups', wombo_views.GroupViewSet)
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
