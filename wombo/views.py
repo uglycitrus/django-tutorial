ea9d7750f1052768b197630cec7beb6e165faa7a
Will O'Meara <womeara@capsule.com>
2023-01-26 12:27:05 -0500

    ready to start on django performance

--- wombo/views.py
+++ wombo/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User, Group
+from rest_framework.renderers import JSONRenderer
 from rest_framework import viewsets
 from rest_framework import permissions
 from wombo.serializers import UserSerializer, GroupSerializer
@@ -11,6 +12,8 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
+    renderer_classes = (JSONRenderer, )
+    pagination_class = None
 
 
 class GroupViewSet(viewsets.ModelViewSet):
